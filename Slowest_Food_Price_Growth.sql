df3c7a2b558097195e1b0372c802c0150e2796bc
Jana Barotová <77615919+akcaloks@users.noreply.github.com>
2025-09-16 10:57:16 +0200

    Update Slowest_Food_Price_Growth.sql
    rozvedená odpověd

--- Slowest_Food_Price_Growth.sql
+++ Slowest_Food_Price_Growth.sql
@@ -28,5 +28,16 @@ SELECT
 FROM price_changes
 GROUP BY category_name
 ORDER BY avg_percentage_change
-LIMIT 1
--- Odpověď: Cukr krystalový – dokonce zlevnil.
+-- LIMIT 1 - limitovalo by na jeden zaznam
+
+-- Odpověď: 
+-- Výsledky ukazují, že ne všechny ceny potravin dlouhodobě rostly, jak je v ekonomice běžné.
+-- Největší výjimkou je „Cukr krystalový“, který v průměru meziročně zlevňoval o 1,9 %. 
+-- Podobně i „Rajská jablka“ (-0,7 %) vykazovala mírný pokles cen. 
+-- Některé položky jako „Banány“ (+0,8 %) prakticky stagnovaly, jejich ceny rostly jen velmi nepatrně. 
+-- Mezi další stabilní komodity (do 1,5 % růstu ročně) patří např. vepřová pečeně, minerální voda nebo šunkový salám. 
+--
+-- Naopak nejrychleji zdražovalo máslo (+6,7 %), vejce (+5,6 %) a některé druhy zeleniny, zejména papriky (+7,3 %) a mrkev (+5,2 %).
+--
+-- Shrnutí: Nejpomaleji zdražovaly cukr (dokonce zlevnil) i rajčata. Dále málo rostly cen banánů (prakticky stagnace), zatímco nejrychleji rostly ceny másla, vejce a zeleniny.
+
